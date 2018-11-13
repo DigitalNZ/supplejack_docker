8946b1565bd8660b6b9f9e73063e566789a75de3
Oliver Stigley <oliver@boost.co.nz>
2018-11-13 17:06:20 +1300

    getting crons to work in api sidekiq to index

--- sample_rails_api/config/schedule.rb
+++ sample_rails_api/config/schedule.rb
@@ -1,15 +1,21 @@
 # Use this file to define when/how often the API background jobs should run.
 # This is an example file that will not work without adding the "whenever" gem to your API project
 # If you wish to use another scheduling system you can use this as a basis
+env :PATH, ENV['PATH']
+env :GEM_PATH, ENV['GEM_PATH']
+env :GEM_HOME, ENV['GEM_HOME']
+env :BUNDLE_BIN_PATH, ENV['BUNDLE_BIN_PATH']
 
-set :environment, :development if Rails.env.development?
-set :output, {:error => 'log/whenever.stderr.log', :standard => 'log/whenever.stdout.log'}
+set :environment, :development
+set :ruby_version, '2.3.1'
+job_type :runner,  "cd :path && bundle exec rails runner -e :environment ':task' :output"
+set :output, {:error => 'log/development.log', :standard => 'log/development.log'}
 
 every '*/5 * * * *' do
   runner 'SupplejackApi::InteractionMetricsWorker.perform_async'
 end
 
-every '*/5 * * * *' do
+every '* * * * *' do
   runner 'SupplejackApi::IndexRemainingRecordsInQueue.perform_async'
 end
 
